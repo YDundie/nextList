4d2b9f56c390b7a03391462b12c47de7b1e23184
YDundie <mustacmarko@yahoo.com>
2021-08-29 13:35:24 +0200

    added components

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+const Footer = () => {
+    return (<div>
+        <p>Copyright 2021 Samurais</p>
+    </div>);
+}
+
+export default Footer;
---
+++ components/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (<nav>
+        <div className="logo">
+            <h1>Samurai List</h1>
+        </div>
+        <a>Home</a>
+        <a>About</a>
+        <a>Samuria Listing</a>
+    </nav>);
+}
+
+export default Navbar;
