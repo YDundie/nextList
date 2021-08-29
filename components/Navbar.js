d7855004ef58b80d8b719852060d5ccc1b45580e
YDundie <mustacmarko@yahoo.com>
2021-08-29 13:41:12 +0200

    Linking

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,13 @@
+import Link from 'next/link'
+
 const Navbar = () => {
     return (<nav>
         <div className="logo">
             <h1>Samurai List</h1>
         </div>
-        <a>Home</a>
-        <a>About</a>
-        <a>Samuria Listing</a>
+        <Link href='/'>Home</Link>
+        <Link href='/about'>About</Link>
+        <Link href='/samurais'>Samuria Listing</Link>
     </nav>);
 }
 
